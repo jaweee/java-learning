f9a1668f25353afd2ec470339c138d94fa386ba0
jaweee <3466473208@qq.com>
2023-02-01 16:25:07 +0800

    redis-hmdp

---
+++ redis-hmdp/src/test/java/com/jaweee/redishmdp/RedisHmdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaweee.redishmdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisHmdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
