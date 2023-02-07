de459cb14ca7fe4d9600858650bbf9f2da6a7e41
jaweee <3466473208@qq.com>
2023-02-07 13:30:17 +0800

    缓存了首页的shop-type

--- hmdp/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hmdp/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -12,6 +13,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -31,14 +34,24 @@ public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> i
     public Result queryList() {
         // 这里做个首页类型缓存
         // 1. 去redis取数据
-        String shopTypeJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_TYPE_KEY);
-        ShopType shopType = null;
-        // 如果存在，就直接返回
-        if (StrUtil.isNotBlank(shopTypeJson)) {
-            shopType = JSONUtil.toBean(shopTypeJson, ShopType.class);
-            return Result.ok(shopType);
+        // String shopTypeJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_TYPE_KEY);
+        List<ShopType> shopTypeList = new ArrayList<>();
+        List<String> shopStringList = stringRedisTemplate.opsForList().range(RedisConstants.CACHE_SHOP_TYPE_KEY, 0, -1);
+        // 2. 如果存在，就直接返回
+        if (!shopStringList.isEmpty()) {
+            for (String item : shopStringList) {
+                shopTypeList.add(JSONUtil.toBean(item, ShopType.class));
+            }
+            return Result.ok(shopTypeList);
         }
-        // 3. 如果不存在，就把
-        return null;
+        // 3. 如果不存在，去数据库中查询
+        shopTypeList = list();
+        if (shopTypeList == null || shopTypeList.isEmpty()) {
+            return Result.fail("暂无资源");
+        }
+        for (ShopType item : shopTypeList) {
+            stringRedisTemplate.opsForList().leftPush(RedisConstants.CACHE_SHOP_TYPE_KEY, JSONUtil.toJsonStr(item));
+        }
+        return Result.ok(shopTypeList);
     }
 }
